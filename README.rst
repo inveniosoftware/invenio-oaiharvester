275bf1f35ce6af3bc15274d504a5b9f4fa028fc5
Jorik van Kemenade <jorikvankemenade@gmail.com>
2019-07-31 16:55:40 +0200

    global: license change to MIT License

--- README.rst
+++ README.rst
@@ -1,25 +1,10 @@
 ..
     This file is part of Invenio.
-    Copyright (C) 2015, 2016 CERN.
+    Copyright (C) 2015-2019 CERN.
 
-    Invenio is free software; you can redistribute it
-    and/or modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+    Invenio is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
 
-    Invenio is distributed in the hope that it will be
-    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Invenio; if not, write to the
-    Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-    MA 02111-1307, USA.
-
-    In applying this license, CERN does not
-    waive the privileges and immunities granted to it by virtue of its status
-    as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 ======================
  Invenio-OAIHarvester
@@ -31,19 +16,13 @@
 .. image:: https://img.shields.io/coveralls/inveniosoftware/invenio-oaiharvester.svg
         :target: https://coveralls.io/r/inveniosoftware/invenio-oaiharvester
 
-.. image:: https://img.shields.io/github/tag/inveniosoftware/invenio-oaiharvester.svg
-        :target: https://github.com/inveniosoftware/invenio-oaiharvester/releases
-
-.. image:: https://img.shields.io/pypi/dm/invenio-oaiharvester.svg
-        :target: https://pypi.python.org/pypi/invenio-oaiharvester
-
-.. image:: https://img.shields.io/github/license/inveniosoftware/invenio-oaiharvester.svg
-        :target: https://github.com/inveniosoftware/invenio-oaiharvester/blob/master/LICENSE
+.. image:: https://img.shields.io/pypi/v/invenio-oaiharvester.svg
+        :target: https://pypi.org/pypi/invenio-oaiharvester
 
 
 Invenio module for OAI-PMH metadata harvesting between repositories.
 
-* Free software: GPLv2 license
+* Free software: MIT license
 * Documentation: https://invenio-oaiharvester.readthedocs.io/
 
 *This is an experimental development preview release.*
--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015-2019 CERN.
 #
-# Invenio is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# Invenio is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
-#
-# In applying this license, CERN does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 """Invenio module for OAI-PMH metadata harvesting between repositories."""
 
@@ -50,13 +34,13 @@ extras_require = {
         'Sphinx>=1.5.3,<1.6',
     ],
     'postgresql': [
-        'invenio-db[postgresql]>=1.0.0a9',
+        'invenio-db[postgresql]>=1.0.0',
     ],
     'mysql': [
-        'invenio-db[mysql]>=1.0.0a9',
+        'invenio-db[mysql]>=1.0.0',
     ],
     'sqlite': [
-        'invenio-db>=1.0.0a9',
+        'invenio-db>=1.0.0',
     ],
     'tests': tests_require,
 }
@@ -93,7 +77,7 @@ setup(
     description=__doc__,
     long_description=readme + '\n\n' + history,
     keywords='invenio TODO',
-    license='GPLv2',
+    license='MIT',
     author='CERN',
     author_email='info@inveniosoftware.org',
     url='https://github.com/inveniosoftware/invenio-oaiharvester',
@@ -119,7 +103,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
@@ -129,7 +113,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Development Status :: 1 - Planning',
+        'Development Status :: 5 - Production/Stable',
     ],
 )
